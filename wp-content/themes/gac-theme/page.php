b84c555a8cdea7293a488a08e9911b8defa71c50
Benjamin Katz <bk@benjaminkatzcreative.com>
2014-11-20 17:02:05 -0800

    test “A”

--- wp-content/themes/gac-theme/page.php
+++ wp-content/themes/gac-theme/page.php
@@ -18,7 +18,7 @@ get_header(); ?>
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div class="post" id="post-<?php the_ID(); ?>">
 				<div class="entry">
-					<?php the_content(); ?>
+					<?php the_content(); ?>A
 					<?php endwhile; endif; ?>
 				</div>
 			</div>
